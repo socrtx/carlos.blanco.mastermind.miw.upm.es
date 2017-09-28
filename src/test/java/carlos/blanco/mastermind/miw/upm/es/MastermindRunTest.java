fd8158c3ed520364eecc876ffd34b77e596eb707
maste <maste@LAPTOP-9Q7VHT04>
2017-09-28 21:19:33 +0200

    Create MasterRunTest

---
+++ src/test/java/carlos/blanco/mastermind/miw/upm/es/MastermindRunTest.java
@@ -0,0 +1,19 @@
+package carlos.blanco.mastermind.miw.upm.es;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MastermindRunTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
