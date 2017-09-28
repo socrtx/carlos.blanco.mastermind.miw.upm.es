cbc624b1afd0c9df20a839e70c2e6931baadf42c
maste <maste@LAPTOP-9Q7VHT04>
2017-09-28 18:27:08 +0200

    Create Score

---
+++ src/main/java/carlos/blanco/mastermind/miw/upm/es/Score.java
@@ -0,0 +1,22 @@
+package carlos.blanco.mastermind.miw.upm.es;
+
+public class Score 
+{
+	private int correctPositionAndColor;
+	private int correctColor;
+
+	public Score(int correctPositionAndColor, int correctColor) 
+	{
+		super();
+		this.correctPositionAndColor = correctPositionAndColor;
+		this.correctColor = correctColor;
+	}
+
+	public int getCorrectPositionAndColor() {
+		return correctPositionAndColor;
+	}
+	
+	public String toString(){
+		return "(Black Flags:" + this.correctPositionAndColor +", White Flags: " + this.correctColor + ")";
+	}
+}
