f3d3134f604045a8a572576739692886800d735c
maste <maste@LAPTOP-9Q7VHT04>
2017-09-28 21:18:27 +0200

    modify Colors

--- src/main/java/carlos/blanco/mastermind/miw/upm/es/Colors.java
+++ src/main/java/carlos/blanco/mastermind/miw/upm/es/Colors.java
@@ -1,59 +1,45 @@
 package carlos.blanco.mastermind.miw.upm.es;
 
-public enum Colors 
-{
-	RED(0,'r'),GREEN(1,'g'),BLUE(2,'b'),YELLOW(3,'y'),PURPLE(4,'p'),ORANGE(5,'o');
-	
-	private int number;
-	private char character;
-	
-	Colors(int number,char character)
-	{
-		this.number=number;
-		this.character= character;
-	}
-	
-	
-	public static Colors generate (int number)
-	{
-		for(Colors color :Colors.values())
-		{
-			if(color.getNumber() == number)
-			{
-				return color;
-			}
-		}
-		return null;
-	}
-	
-	public static Colors generate(char character)
-	{
-		for(Colors color :Colors.values())
-		{
-			if(color.getCharacter() == character)
-			{
-				return color;
-			}
-		}
-		return null;
-	}
+public enum Colors {
+    RED(0, 'r'), GREEN(1, 'g'), BLUE(2, 'b'), YELLOW(3, 'y'), PURPLE(4, 'p'), ORANGE(5, 'o');
 
+    private int number;
 
+    private char character;
 
-	public int getNumber() 
-	{
-		return number;
-	}
+    Colors(int number, char character) {
+        this.number = number;
+        this.character = character;
+    }
 
-	public char getCharacter() 
-	{
-		return character;
-	}
-	
-	public String toString()
-	{
-		return this.name();
-	}
+    public static Colors generate(int number) {
+        for (Colors color : Colors.values()) {
+            if (color.getNumber() == number) {
+                return color;
+            }
+        }
+        return null;
+    }
 
+    public static Colors generate(char character) {
+        for (Colors color : Colors.values()) {
+            if (color.getCharacter() == character) {
+                return color;
+            }
+        }
+        return null;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public char getCharacter() {
+        return character;
+    }
+
+    public String toString() {
+        return this.name();
+    }
 
 }
